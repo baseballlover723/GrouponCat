801406d4b79e7f5b5a3e2578441c722167b35b0e
phross <ross@rose-hulman.edu>
2015-07-20 14:13:59 -0500

    initial commit

---
+++ wear/src/main/java/com/example/phross/grouponcat/MyStubBroadcastActivity.java
@@ -0,0 +1,22 @@
+package com.example.phross.grouponcat;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+/**
+ * Example shell activity which simply broadcasts to our receiver and exits.
+ */
+public class MyStubBroadcastActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent i = new Intent();
+        i.setAction("com.example.phross.grouponcat.SHOW_NOTIFICATION");
+        i.putExtra(MyPostNotificationReceiver.CONTENT_KEY, getString(R.string.title));
+        sendBroadcast(i);
+        finish();
+    }
+}
